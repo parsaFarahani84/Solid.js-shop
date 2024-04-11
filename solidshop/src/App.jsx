6a29ccfb486eb0b09d911047416192c6465375b0
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-11 16:58:33 +0330

    ...

--- solidshop/src/App.jsx
+++ solidshop/src/App.jsx
@@ -1,10 +1,32 @@
 import Card from "./components/Card/Card";
+import { createSignal } from "solid-js";
+import "boxicons";
 
 function App() {
+  const [txt, setTxt] = createSignal("Welcome To Shop");
+  const [dark, setDark] = createSignal(false);
+
+  setTimeout(() => {
+    setTxt("Let's Start");
+  }, 1500);
+
+  function changeMode() {
+    setDark((preve) => !preve);
+    console.log(dark());
+  }
+
   return (
-    <div className="container m-auto">
+    <div className="containe">
+      <span className="darkmode" onclick={changeMode}>
+        {dark() ? (
+          <box-icon name="moon" type="solid"></box-icon>
+        ) : (
+          <box-icon type="solid" name="sun"></box-icon>
+        )}
+        {dark() ? <p>Dark Mode</p> : <p>Light Mode</p>}
+      </span>
       <div class="banner">
-        <h1>Welcome To Shop</h1>
+        <h1>{txt()}</h1>
       </div>
       <div class="gridCard">
         <Card title="Less Price" />
