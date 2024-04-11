d8a06bb905704a394a88159ccdc51c5141363af5
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-11 19:07:40 +0330

    ...

--- solidshop/src/App.jsx
+++ solidshop/src/App.jsx
@@ -1,47 +1,15 @@
-import { createSignal } from "solid-js";
 import { Route, Router, A } from "@solidjs/router";
-import "boxicons";
+
 import Home from "./pages/Home/Home";
+import Nav from "./components/Nav";
 
 function App() {
-  const [dark, setDark] = createSignal(true);
-
-  function changeMode() {
-    setDark((preve) => !preve);
-    console.log(dark());
-  }
-
   return (
-    <div className={`containe ${dark() ? "dark" : ""}`}>
-      {/* -------------------------------------- */}
-
-      <nav>
-        <span
-          className={`darkmode ${dark() ? "light" : ""}`}
-          onclick={changeMode}
-        >
-          {dark() ? (
-            <box-icon name="moon" type="solid" color="#fff"></box-icon>
-          ) : (
-            <box-icon type="solid" name="sun"></box-icon>
-          )}
-          {dark() ? <p>Dark Mode</p> : <p>Light Mode</p>}
-        </span>
-        <span>
-          <A href="./">Home</A>
-        </span>
-        <span>
-          <A href="./cart">Cart</A>
-        </span>
-      </nav>
-      {/* -------------------------------------- */}
-      <div class="banner">
-        <h1>Welcome To Shop</h1>
-      </div>
+    <Nav>
       <Router>
         <Route path="/" component={Home} />
       </Router>
-    </div>
+    </Nav>
   );
 }
 
---
+++ solidshop/src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import { createSignal } from "solid-js";
+import "boxicons";
+// import { A } from "@solidjs/router";
+
+function Nav(props) {
+  const [dark, setDark] = createSignal(true);
+
+  function changeMode() {
+    setDark((preve) => !preve);
+    console.log(dark());
+  }
+
+  return (
+    <div className={`containe ${dark() ? "dark" : ""}`}>
+      {/* -------------------------------------- */}
+      <nav>
+        <span
+          className={`darkmode ${dark() ? "light" : ""}`}
+          onclick={changeMode}
+        >
+          {dark() ? (
+            <box-icon name="moon" type="solid" color="#fff"></box-icon>
+          ) : (
+            <box-icon type="solid" name="sun"></box-icon>
+          )}
+          {dark() ? <p>Dark Mode</p> : <p>Light Mode</p>}
+        </span>
+        <span>
+          <a href="./">Home</a>
+        </span>
+        <span>
+          <a href="./cart">Cart</a>
+        </span>
+      </nav>
+      {/* -------------------------------------- */}
+      <div class="banner">
+        <h1>Welcome To Shop</h1>
+      </div>
+      {props.children}
+    </div>
+  );
+}
+
+export default Nav;
