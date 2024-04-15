caa5ffe5eb9418208a71533ad69091b590cca04c
Parsa Farahani <parsa.farahani84@gmail.com>
2024-04-15 16:56:17 +0330

    ...

--- solidshop/src/pages/TheProduct/TheProduct.jsx
+++ solidshop/src/pages/TheProduct/TheProduct.jsx
@@ -11,23 +11,34 @@ function TheProduct() {
   const [datas, setData] = useAtom(datastore);
 
   function addProduct(id) {
-    const selected = data.products.filter((product) => product.id === id);
-    // -------------------------------------------------------------
-    // Assuming only one product will have a unique ID
-    const productToAdd = selected[0];
+    const selected = data.products.find((product) => product.id === id);
+    if (!selected) return; // Exit if product is not found
 
-    setData((oldProductData) => [
-      ...oldProductData,
-      {
-        title: productToAdd.title,
-        id: productToAdd.id,
-        img: productToAdd.img,
-        price: productToAdd.price,
-        number: click(),
-      },
-    ]);
+    // Check if the product is already in the datas array
+    const existingProductIndex = datas.findIndex(
+      (product) => product.id === id
+    );
 
-    // -------------------------------------------------------------
+    // If the product is already in the datas array, update its quantity
+    if (existingProductIndex !== -1) {
+      setData((oldProductData) => {
+        const updatedProductData = [...oldProductData];
+        updatedProductData[existingProductIndex].number += click();
+        return updatedProductData;
+      });
+    } else {
+      // If the product is not in the datas array, add it
+      setData((oldProductData) => [
+        ...oldProductData,
+        {
+          title: selected.title,
+          id: selected.id,
+          img: selected.img,
+          price: selected.price,
+          number: click(),
+        },
+      ]);
+    }
   }
 
   return (
